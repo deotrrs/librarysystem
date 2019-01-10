17a92d86debd3f6a8cadf171e97ceaf943c6aea6
ojt <djtores102@gmail.com>
2019-01-10 08:09:37 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@
 //     return redirect(route('view'));
 // });
 
-Route::get('/', 'UserController@index')->name('view');
+// Route::get('/', 'UserController@index')->name('view');
 
-// Route::get('/', function(){
-//     return view('index');
-// })->name('view');
+Route::get('/', function(){
+    return view('index');
+})->name('view');
 
 Route::get('/table', function(){
     return view('books.react');
